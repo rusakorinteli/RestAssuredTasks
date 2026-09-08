865cdb0abd623fc965d2759828479841085efd5b
Rusa Korinteli <rkorinteli@credo.ge>
2026-09-08 17:31:16 +0400

    Task 5 - Users validation

--- src/main/java/Models/ReqRes/UserResponse/UserModel.java
+++ src/main/java/Models/ReqRes/UserResponse/UserModel.java
@@ -1,4 +1,9 @@
 package Models.ReqRes.UserResponse;
 
 public class UserModel {
+    public int id;
+    public String email;
+    public String first_name;
+    public String last_name;
+    public String avatar;
 }
